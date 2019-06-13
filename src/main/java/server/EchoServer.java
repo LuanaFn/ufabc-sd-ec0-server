d8ce59ecd2af850e4d3f20345deb4cdb06985bf5
luana <lluana13@hotmail.com>
2019-06-13 20:57:26 -0300

    tentando ordenar mensagens

--- src/main/java/server/EchoServer.java
+++ src/main/java/server/EchoServer.java
@@ -1,47 +1,60 @@
 package main.java.server;
+
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
 
 import org.apache.log4j.Logger;
 
 public class EchoServer extends Thread {
 	private DatagramSocket socket;
-    private boolean running;
-    private byte[] buf = new byte[256];
-    Logger log = Logger.getLogger(EchoServer.class);;
- 
-    public EchoServer() throws SocketException {
-        socket = new DatagramSocket(4445);
-    }
- 
-    public void run(){
-        running = true;
- 
-        while (running) {
-        	try{
-            DatagramPacket packet 
-              = new DatagramPacket(buf, buf.length);
-            socket.receive(packet);
-             
-            InetAddress address = packet.getAddress();
-            int port = packet.getPort();
-            packet = new DatagramPacket(buf, buf.length, address, port);
-            String received 
-              = new String(packet.getData(), 0, packet.getLength());
-             
-            if (received.equals("end")) {
-                running = false;
-                continue;
-            }
-            socket.send(packet);
-        	}
-        	catch (Exception ex)
-        	{
-        		log.error(ex);
-        	}
-        }
-        socket.close();
-    }
+	private boolean running;
+	private byte[] buf = new byte[256];
+	Timer timer = new Timer();
+	TimerResposta timerR = new TimerResposta();
+	Logger log = Logger.getLogger(EchoServer.class);
+	
+	List recebidos = new ArrayList<String>();
+
+	public EchoServer() throws SocketException {
+		
+		timer.schedule(timerR, 0, 5000);
+		socket = new DatagramSocket(4445);
+	}
+
+	public void run() {
+		running = true;
+
+		while (running) {
+			try {
+				DatagramPacket packet = new DatagramPacket(buf, buf.length);
+				socket.receive(packet);
+
+				InetAddress address = packet.getAddress();
+				int port = packet.getPort();
+				packet = new DatagramPacket(buf, buf.length, address, port);
+				String received = new String(packet.getData(), 0, packet.getLength());
+
+				System.out.println("Recebeu a mensagem: " + received);
+
+				if (received.equals("end")) {
+					running = false;
+					continue;
+				}
+				
+				recebidos.add(received);
+				
+				//socket.send(packet);
+				
+				timerR.config(recebidos, socket, packet);
+			} catch (Exception ex) {
+				log.error(ex);
+			}
+		}
+		socket.close();
+	}
 }
---
+++ src/main/java/server/TimerResposta.java
@@ -0,0 +1,58 @@
+package main.java.server;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.TimerTask;
+
+public class TimerResposta extends TimerTask {
+
+	private List<String> recebidos = new ArrayList<String>();
+	private DatagramSocket socket;
+	private DatagramPacket packet;
+	String prefixo = "!////MENSAGEM COMECA AQUI////!";
+
+	public void config(List recebidos, DatagramSocket socket, DatagramPacket packet) {
+		this.recebidos.addAll(recebidos);
+		this.socket = socket;
+		this.packet = packet;
+	}
+
+	public void run() {
+
+		if (!recebidos.isEmpty()) {
+			Collections.sort(recebidos);
+
+			try {
+
+				//envia cada mensagem de volta ao cliente
+				for (int i = 0; i < recebidos.size(); i++) {
+					String salvo = recebidos.get(i);
+					
+					//captura apenas a mensagem
+					String msg = salvo.split(prefixo)[1];
+					
+					InetAddress address = packet.getAddress();
+					int port = packet.getPort();
+					
+					byte[] buf = msg.getBytes();
+
+					packet = new DatagramPacket(buf, buf.length, address, port);
+
+					socket.send(packet);
+					
+					recebidos.remove(i);
+					
+					System.out.println("Enviou a mensagem: " + msg);
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
